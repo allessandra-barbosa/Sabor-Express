7e462bf621d9e99cae71c731937957ad5fc76838
ale-barbosa <allessandra_barbosa@outlook.com>
2024-05-24 21:14:07 -0300

    .

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def exibir_nome_do_prograna():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
-    print('3. Ativar restaurante')
+    print('3. Alternar estado restaurante')
     print('4. Sair\n')
 
 def finalizar_app():
@@ -25,19 +25,22 @@ def opcao_invalida():
     voltar_ao_menu_princial()
 
 def exibir_subtitulo(texto):
-     os.system('cls')
-     print(texto)
-     print()
+    os.system('cls')
+    linha = '*' (len(texto))
+    print (linha)
+    print(texto)
+    print (linha)
+    print()
 
 def cadastrar_novo_restaurante():
-     exibir_subtitulo('Cadastro de novos restaurantes')
-     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
-     categoria = input(f'Disigite o nome da categoria do restaurante {nome_do_restaurante}: ')
-     dados_do_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria, 'ativo':False}
-     restaurantes.append(dados_do_restaurante)
-     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    categoria = input(f'Disigite o nome da categoria do restaurante {nome_do_restaurante}: ')
+    dados_do_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria, 'ativo':False}
+    restaurantes.append(dados_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
      
-     voltar_ao_menu_princial()
+    voltar_ao_menu_princial()
 
 def listar_restaurantes():
     exibir_subtitulo('Listando os restaurantes')
@@ -45,8 +48,26 @@ def listar_restaurantes():
     for restaurante in restaurantes:
         nome_restaurante = restaurante['nome']
         categoria = restaurante['categoria']
-        ativo = restaurante['ativo']
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
         print(f'- {nome_restaurante} | {categoria} | {ativo}')
+
+    voltar_ao_menu_princial()
+
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alternando estado do restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+          if nome_restaurante == restaurante['nome']:
+               restaurante_encontrado = True
+               restaurante['ativo'] = not restaurante['ativo']
+               mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+               print(mensagem)
+    
+    if not restaurante_encontrado():
+        print('O restaurante não foi encontrado')
+     
 
     voltar_ao_menu_princial()
 
@@ -59,7 +80,7 @@ def escolher_opcao():
         elif opcao_escolhida == 2: 
             listar_restaurantes()
         elif opcao_escolhida == 3: 
-            print('Ativar restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4: 
             finalizar_app()
         else:
@@ -68,10 +89,10 @@ def escolher_opcao():
         opcao_invalida()
 
 def main():
-            os.system('cls')
-            exibir_nome_do_prograna()
-            exibir_opcoes()
-            escolher_opcao()
+    os.system('cls')
+    exibir_nome_do_prograna()
+    exibir_opcoes()
+    escolher_opcao()
 
 
 if __name__ == '__main__':
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,107 +1,99 @@
-"""
+import os
 
-print('# Exercicios modulo 01')
+restaurantes = [{'nome':'Praça', 'categoria':'Japonesa', 'ativo':False},
+                {'nome':'Mutio Suprema', 'categoria':'Italiana', 'ativo':True},
+                {'nome':'Comida Boa', 'categoria':'Brasileira', 'ativo':True}]
 
-print('1. Python na Escola de Programação da Alura')
+def exibir_nome_do_prograna():
+    print('𝚂𝚊𝚋𝚘𝚛 𝙴𝚡𝚙𝚛𝚎𝚜𝚜\n')
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Alternar restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    exibir_subtitulo('Finalizando o app...')
+
+def voltar_ao_menu_princial():
+    input('\nDigite uma tecla para voltar ao menu principal')
+    main()
+
+def opcao_invalida():
+    print('Opção inválida \n')
+    voltar_ao_menu_princial()
+
+def exibir_subtitulo(texto):
+     os.system('cls')
+     linha = '*' (len(texto))
+     print (linha)
+     print(texto)
+     print (linha)
+     print()
+
+def cadastrar_novo_restaurante():
+     exibir_subtitulo('Cadastro de novos restaurantes')
+     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+     categoria = input(f'Disigite o nome da categoria do restaurante {nome_do_restaurante}: ')
+     dados_do_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria, 'ativo':False}
+     restaurantes.append(dados_do_restaurante)
+     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+     
+     voltar_ao_menu_princial()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando os restaurantes')
+
+    for restaurante in restaurantes:
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f'- {nome_restaurante} | {categoria} | {ativo}')
+
+    voltar_ao_menu_princial()
+
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alternando estado do restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+          if nome_restaurante == restaurante['nome']:
+               restaurante_encontrado = True
+               restaurante['ativo'] = not restaurante['ativo']
+               mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+               print(mensagem)
+    
+    if not restaurante_encontrado():
+        print('O restaurante não foi encontrado')
+     
+
+    voltar_ao_menu_princial()
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+
+        if opcao_escolhida == 1: 
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2: 
+            listar_restaurantes()
+        elif opcao_escolhida == 3: 
+            alternar_estado_restaurante()
+        elif opcao_escolhida == 4: 
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+def main():
+            os.system('cls')
+            exibir_nome_do_prograna()
+            exibir_opcoes()
+            escolher_opcao()
 
 
-nome = 'Alessandra'
-idade = '23'
-print(f'2. Meu nome é {nome} e tenho {idade} anos')
-
-
-print('''3. 
-    A
-    L
-    U
-    R
-    A''')
-
-
-pi = 3.14159
-print(f'4. O valor arredondado de pi é: {pi:.2f}')
-
-
-#------------------------------------------------------
-
-
-print('# Exercicios modulo 02')
-
-num_digitado = int(input('1. Digite um número: '))
-
-if num_digitado % 2 == 0:
-    print('O número digitado é par')
-else:
-    print('O número digitado é impar')
-
-
-idade = int(input('Digite sua idade: '))
-
-if idade >= 0 <= 12:
-    print('Sua categoria é criança')
-elif 12 < idade < 18:
-    print('Sua categoria é adolecente')
-else:
-    print('Sua categoria é adulto')
-
-"""
-
-#------------------------------------------------------
-
-
-print('# Exercicios modulo 02\n')
-
-print('1. Crie uma lista\n')
-lista_de_num = [1,2,3,4,5,6,7,8,9,10]
-lista_de_nomes = ['maria','jose','marcio','marcia']
-lista_ano = [2000, 2024]
-
-print('2. Percorra a lista')
-for numero in lista_de_num:
-    print(numero)
-    print()
-
-print('3. Soma números ímpares')
-soma_impares = 0
-for i in range(1, 11, 2):
-    soma_impares += i
-print(soma_impares)
-print()
-
-print('4. Números de 1 a 10 em ordem decrescente')
-for i in range(10, 0, -1):
-    print(i)
-    print()
-
-print('5. Tabuada interativa')
-numero_tabuada = int(input('Digite um número para a tabuada: '))
-for i in range(1, 11):
-    resultado = numero_tabuada * i
-    print(f'{numero_tabuada} x {i} = {resultado}')
-    print()
-
-print('6. Soma de elementos')
-lista_numeros = [10, 5, 8, 3, 7]
-soma = 0
-
-try:
-    for numero in lista_numeros:
-        soma += numero
-    print(f'Soma dos elementos: {soma}')
-except Exception as e:
-    print(f'Ocorreu um erro: {e}')
-    print()
-
-print('7. Validação de lista vazia')
-lista_valores = [15, 20, 25, 30]
-soma_valores = 0
-
-try:
-    for valor in lista_valores:
-        soma_valores += valor
-    media = soma_valores / len(lista_valores)
-    print(f'Média dos valores: {media}')
-except ZeroDivisionError:
-    print('A lista está vazia, não é possível calcular a média')
-except Exception as e:
-    print(f'Ocorreu um erro:{e}')
+if __name__ == '__main__':
+    main()
