023df2887d230e93706d4bfe4d3a55330b1ddf9d
ale-barbosa <allessandra_barbosa@outlook.com>
2024-05-27 20:45:27 -0300

    corrigidos erros

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def exibir_subtitulo(texto):
 def cadastrar_novo_restaurante():
     exibir_subtitulo('Cadastro de novos restaurantes')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
-    categoria = input(f'Disigite o nome da categoria do restaurante {nome_do_restaurante}: ')
+    categoria = input(f'Digite o nome da categoria do restaurante{nome_do_restaurante}: ')
     dados_do_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria, 'ativo':False}
     restaurantes.append(dados_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
@@ -45,7 +45,7 @@ def cadastrar_novo_restaurante():
 def listar_restaurantes():
     exibir_subtitulo('Listando os restaurantes')
 
-    print(f'{'Nome do restaurante'.ljust(22)} | {'Categoria'.ljust(20)}' | 'Status')
+    print(f'{'Nome do restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | Status')
     for restaurante in restaurantes:
         nome_restaurante = restaurante['nome']
         categoria = restaurante['categoria']
